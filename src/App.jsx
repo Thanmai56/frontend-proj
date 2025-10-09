c0ae79744bab7911feb36f90cbe3a39693fbca4f
Thanmai56 <boyapati.thanmaisree@gmail.com>
2025-10-09 15:51:45 +0530

    changes vite config'

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import StudentDashboard from './pages/StudentDashboard'
 import InstructorDashboard from './pages/InstructorDashboard'
-import Practise from './pages/userprofile';
+import Practise from './pages/userprofile'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/student" element={<StudentDashboard />} />
-          <Route path="/instructor" element={<InstructorDashboard />} />
-          <Route path="/practise" element={
-            
-                    <Practise/>
-          
-            }/>
-         
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/instructor" element={<InstructorDashboard />} />
+        <Route path="/practise" element={<Practise />} />
+      </Routes>
+    </div>
   )
 }
 
